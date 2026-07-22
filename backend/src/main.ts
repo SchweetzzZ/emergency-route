8e607fb5fd4845ae2b43c5ed5c5deb8bde3626d9
SchweetzzZ <jonatas_kp32@hotmail.com>
2026-07-22 09:27:59 -0300

    feat(swagger): add swagger openAPI

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
     origin: "*",
     credentials: true,
   })
+  const config = new DocumentBuilder()
+    .setTitle('Emergency API')
+    .setDescription('Documentação da API de emergência')
+    .setVersion('1.0')
+    .addTag('Emergency')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
